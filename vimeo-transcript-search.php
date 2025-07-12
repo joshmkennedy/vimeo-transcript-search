9ba710ef2e0897d5ffcff316b22e16d8700e13fe
josh kennedy <jkennedy@ksensetech.com>
2025-07-12 14:29:04 -0400

    new project

---
+++ vimeo-transcript-search.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Vimeo Transcript Search
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     vimeo-transcript-search
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Vimeo_Transcript_Search
+ */
+
+// Your code starts here.
