f2c27b5fc1c7de33421c18cc80ba6b0f6afd1566
josh kennedy <jkennedy@ksensetech.com>
2025-11-01 13:27:05 -0400

    fix email sending fail

--- src/Services/Email/EmailService.php
+++ src/Services/Email/EmailService.php
@@ -10,10 +10,9 @@ class EmailService implements EmailServiceInterface {
     public function send($email, $subject, $content, $headers) {
         $res = wp_mail($email, $subject, $content, $headers);
         if (!$res) {
-            error_log("Failed to send email", print_r([
+            error_log("Failed to send email: " . print_r([
                 'email' => $email,
                 'subject' => $subject,
-                'content' => $content,
                 'headers' => $headers,
             ], true));
         }
